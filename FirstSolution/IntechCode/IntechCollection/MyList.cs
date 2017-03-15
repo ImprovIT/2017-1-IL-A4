89b9bb6d9b9ce393f248dc6cef43c813b81b1b3b
Alexandre <alexandre.lim@hotmail.com>
2017-03-15 16:37:20 +0100

    Implémentation des TU et de la MyList

--- FirstSolution/IntechCode/IntechCollection/MyList.cs
+++ FirstSolution/IntechCode/IntechCollection/MyList.cs
@@ -4,32 +4,87 @@ namespace IntechCode.IntechCollection
 {
     public class MyList<T> : IMyList<T>
     {
-        public T this[int index] 
+        private int _sizeArray = 5;
+        private T[] _array;
+        private int _currentIndex;
+
+        public MyList()
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            this._array = new T[this._sizeArray];
+            this._currentIndex = 0;
+        }
+        public MyList(int sizeArray)
+        {
+            this._array = new T[sizeArray];
+            this._currentIndex = 0;
         }
 
-        public int Count => throw new NotImplementedException();
+        public T this[int index] 
+        {
+            get => this._array[index];
+            set => this._array[index] = value;
+        }
+
+        public int Count => this._currentIndex;
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            if (NeedToExpandArray())
+                ExpandArray();
+            this._array[_currentIndex] = item;
+            this._currentIndex++;
         }
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            for(int i=0; i < this._currentIndex - 1; i++)
+            {
+                if (item.Equals(this._array[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            if(index <= this.Count)
+            {
+                if (NeedToExpandArray())
+                    ExpandArray();
+                for (int i = this._currentIndex; i > index; i--)
+                {
+                    this._array[i + 1] = this._array[i];
+                }
+                this._array[index] = item;
+                this._currentIndex++;
+            }
+            //throw exception
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            //TO DO
+            if (index <= this.Count)
+            {
+                this._array[index] = default(T);
+            }
+        }
+
+        private bool NeedToExpandArray()
+        {
+            return this._currentIndex+1 == this._array.Length;
+        }
+
+        private void ExpandArray()
+        {
+            T[] tmpArray = new T[this._array.Length * + 1];
+            for(int i = 0; i<this._array.Length; i++)
+            {
+                tmpArray[i] = this._array[i];
+            }
+            this._array = tmpArray;
         }
     }
 }
---
+++ FirstSolution/Tests/IntechCode.Tests/MyListTests.cs
@@ -0,0 +1,72 @@
+﻿using IntechCode.IntechCollection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IntechCode.Tests
+{
+    [NUnit.Framework.TestFixture]
+    public class MyListTests
+    {
+        [NUnit.Framework.Test]
+        [Fact]
+        public void MyListIsInitialEmpty()
+        {
+            //Arrange
+            var myList = new MyList<int>();
+            //Act
+
+            //Assert
+            Assert.Equal(myList.Count, 0);
+        }
+
+        [NUnit.Framework.Test]
+        [Fact]
+        public void AddItemToMyListShouldReturnFive()
+        {
+            //Arrange
+            var myList = new MyList<int>();
+            //Act
+            myList.Add(5);
+            //Assert
+            Assert.Equal(myList[0], 5);
+        }
+
+        [NUnit.Framework.Test]
+        [Fact]
+        public void RemoveFirstItemToMyListShouldReturnZero()
+        {
+            var myList = new MyList<int>();
+            myList.Add(5);
+            Assert.Equal(myList[0], 5);
+            myList.RemoveAt(0);
+            Assert.Equal(myList.Count, 0);
+        }
+
+
+        [NUnit.Framework.Test]
+        [Fact]
+        public void InsertItemToMyListShouldReturnFive()
+        {
+            var myList = new MyList<int>();
+            myList.Insert(0, 5);
+            Assert.Equal(myList[0], 5);
+        }
+
+        public void RetrieveIndexOfItemToMyListShouldReturnZero()
+        {
+            var myList = new MyList<int>();
+            myList.Insert(0, 5);
+            Assert.Equal(myList[0], 5);
+            int index = myList.IndexOf(5);
+            Assert.Equal(index, 0);
+        }
+        public void RetrieveIndexOfNoExistItemToMyListShouldReturnMinusOne()
+        {
+            var myList = new MyList<int>();
+            int index = myList.IndexOf(5);
+            Assert.Equal(index, -1);
+        }
+    }
+}
