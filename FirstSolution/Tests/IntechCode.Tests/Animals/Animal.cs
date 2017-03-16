6bfb59d20e645c76751a3bb824797d1daf83bca2
Alexandre <alexandre.lim@hotmail.com>
2017-03-16 10:58:25 +0100

    Inheritance, type substitution, covariance

---
+++ FirstSolution/Tests/IntechCode.Tests/Animals/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntechCode.Tests.Animals
+{
+    abstract class Animal
+    {
+        public String Name { get; set; }
+
+        public abstract string Shout();
+
+    }
+}
---
+++ FirstSolution/Tests/IntechCode.Tests/Animals/AnimalTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using IntechCode.IntechCollection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IntechCode.Tests.Animals
+{
+    [NUnit.Framework.TestFixture]
+    public class AnimalTests
+    {
+        [NUnit.Framework.Test]
+        [Fact]
+        public void The_array_is_not_a_safe_beast()
+        {
+            //Arrange
+            Animal[] animals = new Animal[5];
+            //Act
+            animals[0] = new Dog();
+            animals[1] = new Cat();
+
+            object[] a0 = animals;
+
+            Action bug = () => a0[2] = "Hello";
+            //Assert
+            bug.ShouldThrow<ArrayTypeMismatchException>();
+        }
+
+        public void co_and_contre_variance()
+        {
+            IMyEnumerator<Dog> eDog =  null;
+            IMyEnumerator<Animal> eAnimal = null;
+            DumpAnimals(eAnimal);
+            DumpAnimals(eDog);
+        }
+
+        static void DumpAnimals(IMyEnumerator<Animal> animals)
+        {
+
+        }
+
+        static void DumpDogs(IMyEnumerator<Dog> dogs)
+        {
+
+        }
+    }
+}
