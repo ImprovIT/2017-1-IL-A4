fcc7e7dbf5058e4118f44b555fd6e04ebd8e8363
Alexandre <alexandre.lim@hotmail.com>
2017-03-16 12:52:10 +0100

    Define IMyDictionary

--- FirstSolution/IntechCode/IntechCollection/IMyDictionary.cs
+++ FirstSolution/IntechCode/IntechCollection/IMyDictionary.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace IntechCode.IntechCollection
 {
-    public interface IMyDictionary
+    public interface IMyDictionary<TKey,TValue>
     {
+        /// <summary>
+        /// Add e key-value pair. The key must not already exist.
+        /// </summary>
+        /// <param name="key">The key to add</param>
+        /// <param name="value">The value to add</param>
+        void Add(TKey key, TValue value);
+
+        bool ContainsKey(TKey key);
+
+        /// <summary>
+        /// Sets the value associated to a key
+        /// When the key does not exist <see cref="KeyNotFoundException"/>
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        TValue this[TKey key] { get;set; }
+
+        bool TryGetValue(TKey key, out TValue value);
+
+        int Count { get; }
+
+        bool Remove(TKey key);
+
     }
 }
---
+++ FirstSolution/Tests/IntechCode.Tests/MyRefOrOutTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace IntechCode.Tests
+{
+    public class MyRefOrOutTests
+    {
+        [Fact]
+        public void typeref_demo()
+        {
+            int i = 0;
+            //Il faut mettre le ref sinon ça ne fonctionne pas
+            IchangeMyParam(ref i);
+            i.Should().Be(950);
+            //Il faut mettre le out
+            IInitializeMyParam(out i);
+            int j;
+            IInitializeMyParam(out j);
+        }
+
+        static void IchangeMyParam(ref int param)=> param += 950;
+
+        static void IInitializeMyParam(out int param) => param = 123;
+
+    }
+}
